3cacf36a3f1f0205ad5540f3f0c291858b8f373e
aryank2804 <160916963+aryank2804@users.noreply.github.com>
2025-11-16 04:40:00 +0000

    Deploying to gh-pages from @ aryank2804/aryank2804.github.io@4c17d5bba136a128c207ec82e34464faabe270ac 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "aryank2804/ar28.github.io",
+    "data-repo": "aryank2804/aryank2804.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
